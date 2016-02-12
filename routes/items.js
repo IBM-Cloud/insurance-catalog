ff56703ea2fece2e13a8a4844c11cc806a81ced5
Grant Gayed <grant_gayed@ca.ibm.com>
2016-02-12 12:36:21 -0500

    Revert "Revert "Revert "clean up lint errors and warnings"""
    This reverts commit 6623f998e99d2083c93ec2b7140d562b3840e144.

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,6 @@
-/*eslint-env node */
 var http = require('http');
-var db = require('./db');
-db.initDB();
+require('./db');
+initDB();
 var USE_FASTCACHE = false;
 
 /*
@@ -47,38 +46,39 @@ exports.loadTest = function(req, res) {
 //Create and populate or delete the database.
 exports.dbOptions = function(req, res) {
     var option = req.params.option.toLowerCase();
-    if (option === 'create') {
-        db.cloudant.db.create('items', function(err/*, body*/) {
-            if (!err) {
-                db.populateDB();
+    if(option === 'create'){
+        cloudant.db.create('items', function(err, body){
+            if(!err){
+                populateDB();
                 res.send({msg:'Successfully created database and populated!'});
-            } else {
+            }
+            else{
                 res.send({msg:err});
             }
         });
-    } else if (option === 'delete') {
-        db.cloudant.db.destroy('items', function(err/*, body*/) {
-	        if (!err) {
-	            res.send({msg:'Successfully deleted db items!'});
-	        } else {
-	        	res.send({msg:'Error deleting db items: ' + err});
-        	}
+    }
+    else if(option === 'delete'){
+        cloudant.db.destroy('items',function(err, body){
+        if(!err){
+            res.send({msg:'Successfully deleted db items!'});
+        }
+        else res.send({msg:'Error deleting db items: ' + err});
         });
-    } else {
-    	res.send({msg: 'your option was not understood. Please use "create" or "delete"'});
-	}
-};
+    }
+    else res.send({msg: 'your option was not understood. Please use "create" or "delete"'});    
+}
 
 //Create an item to add to the database.
 exports.create = function(req, res) {
-    db.itemsDb.insert(req.body, function(err/*, body, headers*/) {
-        if (!err) {
-            res.send({msg: 'Successfully created item'});
-        } else {
-            res.send({msg: 'Error on insert, maybe the item already exists: ' + err});
-        }
-    });
-};
+        db.insert(req.body, function (err, body, headers) {
+            if (!err) {
+                res.send({msg: 'Successfully created item'});
+            }
+            else {
+                res.send({msg: 'Error on insert, maybe the item already exists: ' + err});
+            }
+        });
+    }
 
 //find an item by ID.
 exports.find = function(req, res) {
@@ -91,79 +91,82 @@ exports.find = function(req, res) {
     		res.status(200).send({msg: 'all good'});
     	}
         return;
+    } else {
+        db.get(id, { revs_info: false }, function(err, body) {
+            if (!err){
+                res.send(body);
+            }
+            else{
+                res.send({msg:'Error: could not find item: ' + id});
+            }
+        });
     }
-    db.itemsDb.get(id, { revs_info: false }, function(err, body) {
-        if (!err) {
-            res.send(body);
-        } else {
-            res.send({msg:'Error: could not find item: ' + id});
-        }
-    });
-};
+}
 
 //list all the database contents.
 exports.list = function(req, res) {
-    db.itemsDb.list({include_docs: true}, function(err, body/*, headers*/) {
-	    if (!err) {
-	        res.send(body);
-	        return;
-	    }
-	   	res.send({msg:'Error listing items: ' + err});
+    db.list({include_docs: true}, function (err, body, headers) {
+    if (!err) {
+        res.send(body);
+        return;
+    }
+    else res.send({msg:'Error listing items: ' + err});
     });
-};
+}
 
 //update an item using an ID.
 exports.update = function(req, res) {
     var id = req.params.id;
     var data = req.body;
-    db.itemsDb.get(id, {revs_info:true}, function(err, body) {
-        if (!err) {
+    db.get(id,{revs_info:true}, function (err, body) {
+        if(!err){
             data._rev = body._rev;
-            db.itemsDb.insert(data, id, function(err/*, body, headers*/) {
-	            if (!err) {
-	                res.send({msg:'Successfully updated item: ' + JSON.stringify(data)});
-	            } else {
-	            	res.send({msg:'Error inserting for update: ' + err});
-	        	}
+            db.insert(data, id, function(err, body, headers){
+            if(!err){
+                res.send({msg:'Successfully updated item: ' + JSON.stringify(data)});
+            }
+            else res.send({msg:'Error inserting for update: ' + err});
             });
         }
-        else {
-        	res.send({msg:'Error getting item for update: ' + err});
-    	}
+        else res.send({msg:'Error getting item for update: ' + err});
     });
-};
+}
 
 //remove an item from the database using an ID.
-exports.remove =  function(req, res) {
+exports.remove =  function(req, res){
     var id = req.params.id;
-    db.itemsDb.get(id, { revs_info: true }, function(err, body) {
-        if (!err) {
+    db.get(id, { revs_info: true }, function(err, body) {
+        if (!err){
             //console.log('Deleting item: ' + id);
-            db.itemsDb.destroy(id, body._rev, function(err/*, body*/) {
-                if (!err) {
+            db.destroy(id, body._rev, function(err, body){
+                if(!err){
                     res.send({msg:'Successfully deleted item'});
-                } else {
+                }
+                else{
                     res.send({msg:'Error in delete: ' + err});
                 }
-            });
-        } else {
+            })
+        }
+        else{
             res.send({msg:'Error getting item id: ' + err});
         }
     });  
-};
+}
 
 //calculate the fibonacci of 20.
+exports.fib = function(req, res) {
+    res.send({msg:'Done with fibonacci of 20: ' + fib(20)});
+}
+
 var fib = function(n) {
     if (n < 2) {
         return 1;
+    } 
+    else {
+        return (fib(n - 2) + fib(n - 1));
     }
-    return fib(n - 2) + fib(n - 1);
-};
-exports.fib = function(req, res) {
-    res.send({msg:'Done with fibonacci of 20: ' + fib(20)});
-};
-
+}
 
 exports.getFastCache = function() {
     return USE_FASTCACHE;
-};
+}
