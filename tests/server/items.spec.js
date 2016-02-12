c29ac1ff3b2a626dd7ad63372000e2f6db51c7ee
Grant Gayed <grant_gayed@ca.ibm.com>
2016-02-12 12:37:41 -0500

    Revert "adapt tests to items.js changes"
    This reverts commit 7058157b013db0718921467453c0ac57c35cd172.

--- tests/server/items.spec.js
+++ tests/server/items.spec.js
@@ -1,12 +1,11 @@
 (function() {
-​
+
     'use strict';
-​
+
     var rewire = require("rewire");
     var assert = require('chai').assert;
     var sinon = require('sinon');
-​
-/*
+
     var mockcloudant = {
         db: {
             create: function(key, callback) {
@@ -14,24 +13,9 @@
             }
         }
     };
-*/
+
     var mockdb = {
-        itemsDb: {
-            insert: function(body, callback){
-                callback(null);
-            },
-            get: function(id, obj, callback) {
-                callback(null, "body");
-            }
-        },
-        populateDB: function() {},
-        cloudant: {
-            db: {
-                create: function(key, callback) {
-                    callback( false, "woot" );
-                }
-            }
-        }
+        insert: function(body, callback){}
     };    
     
     global.cloudantService = {
@@ -39,23 +23,23 @@
             url: "https://abc"
         }
     };
-​
-    //var cdb = rewire('../../tests/server/coverage/instrumented/routes/db.js');
-    //cdb.__set__('cloudant', mockcloudant);
-    //cdb.__set__('itemsDb', mockdb);
+
+    var cdb = rewire('../../tests/server/coverage/instrumented/routes/db.js');
+    cdb.__set__('cloudant', mockcloudant);
+    cdb.__set__('db', mockdb);
     var items = rewire('../../tests/server/coverage/instrumented/routes/items.js');
-    //items.__set__('cloudant', mockcloudant);
+    items.__set__('cloudant', mockcloudant);
     items.__set__('db', mockdb);
-​
+
     var USE_FASTCACHE = items.getFastCache();
-​
+
     // create mock request and response
     var reqMock = {
         params: {
             option: 'create'
         }
     };
-​
+
     var resMock = {};
     resMock.status = function() { return this;};
     resMock.send = function() {};
@@ -65,9 +49,9 @@
     describe('dbOptions Function', function() {
         it('DB created successfully', function() {
             reqMock.params.option = 'create';
-            //mockcloudant.db.create = function( key, callback ){
-            //    callback( false, '' );  
-            //};
+            mockcloudant.db.create = function( key, callback ){
+                callback( false, '' );  
+            };
             
             items.dbOptions( reqMock, resMock );
             assert( resMock.send.lastCall.calledWith( { msg: 'Successfully created database and populated!' } ), 'Unexpected argument: ' + JSON.stringify(resMock.send.lastCall.args) );
@@ -75,7 +59,7 @@
         
         it('DB not created - cloudant failure', function() {
             reqMock.params.option = 'create';  
-            mockdb.cloudant.db.create = function( key, callback ){
+            mockcloudant.db.create = function( key, callback ){
                 callback( 'Failed to contact server.', '' );  
             };
             
@@ -85,7 +69,7 @@
         
         it('DB deleted successfully', function() {  
             reqMock.params.option = 'delete';  
-            mockdb.cloudant.db.destroy = function( key, callback ){
+            mockcloudant.db.destroy = function( key, callback ){
                 callback( false, '' );  
             };
             
@@ -95,7 +79,7 @@
         
         it('DB not deleted - cloudant failure', function() {
             reqMock.params.option = 'delete';
-            mockdb.cloudant.db.destroy = function( key, callback ){
+            mockcloudant.db.destroy = function( key, callback ){
                 callback( 'Failed to contact server.', '' );  
             };
             
@@ -106,17 +90,17 @@
     
     describe('create Function', function() {
         it('Item created successfully', function() {
-            //items.db.itemsDB.insert = function( key, callback ){
-            //    callback( false, '', '' );  
-            //};
+            mockdb.insert = function( key, callback ){
+                callback( false, '', '' );  
+            };
             
             items.create( reqMock, resMock );
             assert( resMock.send.lastCall.calledWith( { msg: 'Successfully created item' } ), 'Unexpected argument: ' + JSON.stringify(resMock.send.lastCall.args) );
         });
         
         it('Item not created - db error', function() {
-            mockdb.itemsDb.insert = function( key, callback ){
-                callback('forced error');  
+            mockdb.insert = function( key, callback ){
+                callback( 'forced error', '', '' );  
             };
     
             items.create( reqMock, resMock );        
@@ -127,7 +111,7 @@
     describe('find Function', function() {
         it('Item found successfully', function() {
             reqMock.params.id = 'testId';
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( false, 'test body' );  
             };
             
@@ -141,7 +125,7 @@
         
         it('Item not found - db error', function() {
             reqMock.params.id = 'testId';
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( 'forced error', '' );  
             };
             
@@ -156,7 +140,7 @@
     
     describe('list Function', function() {
         it('All Db content listed successfully', function() {
-            mockdb.itemsDb.list = function( arg, callback ){
+            mockdb.list = function( arg, callback ){
                 callback( false, 'test body', 'headers' );  
             };
             
@@ -165,7 +149,7 @@
         });
         
         it('Db content not listed - db error', function() {
-            mockdb.itemsDb.list = function( arg, callback ){
+            mockdb.list = function( arg, callback ){
                 callback( 'forced error', 'test body', 'headers' );  
             };
             
@@ -177,11 +161,11 @@
     describe('update Function', function() {
         it('Item updated successfully', function() {
             reqMock.body = {};
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( false, { _rev: 'test' } );  
             };
             
-            mockdb.itemsDb.insert = function( data, id, callback ){
+            mockdb.insert = function( data, id, callback ){
                 callback( false, '', '' );  
             };
             
@@ -191,11 +175,11 @@
         
         it('Item not updated - error on insert', function() {
             reqMock.body = {};
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( false, { _rev: 'test' } );  
             };
             
-            mockdb.itemsDb.insert = function( data, id, callback ){
+            mockdb.insert = function( data, id, callback ){
                 callback( 'forced error', '', '' );  
             };
             
@@ -206,11 +190,11 @@
         
         it('Item not updated - error on get', function() {
             reqMock.body = {};
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( 'forced error on get', { _rev: 'test' } );  
             };
             
-            mockdb.itemsDb.insert = function( data, id, callback ){
+            mockdb.insert = function( data, id, callback ){
                 callback( false, '', '' );  
             };
             
@@ -221,11 +205,11 @@
     
     describe('remove Function', function() {
         it('Item removed successfully', function() {        
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( false, 'body' );  
             };
             
-            mockdb.itemsDb.destroy = function( id, arg, callback ){
+            mockdb.destroy = function( id, arg, callback ){
                 callback( false, '' );  
             };
             
@@ -234,11 +218,11 @@
         });
         
         it('Item not removed - error on destroy', function() {
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( false, 'body' );  
             };
             
-            mockdb.itemsDb.destroy = function( id, arg, callback ){
+            mockdb.destroy = function( id, arg, callback ){
                 callback( 'forced erro on destroy', '' );  
             };
             
@@ -247,11 +231,11 @@
         });
         
         it('Item not removed - error on get', function() {
-            mockdb.itemsDb.get = function( id, arg, callback ){
+            mockdb.get = function( id, arg, callback ){
                 callback( 'forced error on get', 'body' );  
             };
             
-            mockdb.itemsDb.destroy = function( id, arg, callback ){
+            mockdb.destroy = function( id, arg, callback ){
                 callback( false, '' );
             };
             
