333e1749beefe51aedf82e56fa635da36bde403b
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-02-14 10:55:31 +0100

    moved build scripts into their respective modules https://github.com/IBM-Bluemix/insurance-toolchain/issues/13

---
+++ .bluemix/pipeline-DEPLOY.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# The branch may use a custom manifest
+MANIFEST=manifest.yml
+PREFIX=""
+if [ -f ${REPO_BRANCH}-manifest.yml ]; then
+  MANIFEST=${REPO_BRANCH}-manifest.yml
+  PREFIX=$REPO_BRANCH"-"
+fi
+echo "Using manifest file: $MANIFEST"
+echo "Using prefix: $PREFIX"
+
+# Create CF services
+cf create-service cloudantNoSQLDB Lite ${PREFIX}insurance-policy-db
+cf create-service tradeoff_analytics standard insurance-tradeoff-analytics
+
+if ! cf app $CF_APP; then
+  cf push $CF_APP -n $CF_APP -f $MANIFEST
+else
+  OLD_CF_APP=${CF_APP}-OLD-$(date +"%s")
+  rollback() {
+    set +e
+    if cf app $OLD_CF_APP; then
+      cf logs $CF_APP --recent
+      cf delete $CF_APP -f
+      cf rename $OLD_CF_APP $CF_APP
+    fi
+    exit 1
+  }
+  set -e
+  trap rollback ERR
+  cf rename $CF_APP $OLD_CF_APP
+  cf push $CF_APP -n $CF_APP -f $MANIFEST
+  cf delete $OLD_CF_APP -f
+fi
